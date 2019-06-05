219cd5a07f61d105f1c7bba2e7ae4d8d4131c5f3
ram.kuppusamy <Welcome12345&>
2019-06-05 01:08:12 -0400

    register fcm token working

--- android/app/src/main/java/com/aiwatch/firebase/FirebaseUserDataManager.java
+++ android/app/src/main/java/com/aiwatch/firebase/FirebaseUserDataManager.java
@@ -1,34 +1,19 @@
 package com.aiwatch.firebase;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.WorkerThread;
-
 import com.aiwatch.Logger;
 import com.aiwatch.media.db.CameraConfig;
 import com.aiwatch.media.db.CameraConfigDao;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class FirebaseUserDataManager {
 
@@ -39,16 +24,10 @@ public class FirebaseUserDataManager {
     // an IllegalStateException will be thrown.
     public void registerToken(Context context, String token) {
         try{
-           GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(context);
-           LOGGER.d("firebaseAuthWithGoogle:" + account.getId());
-           FirebaseAuth mAuth = FirebaseAuth.getInstance();
-           AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-           Task<AuthResult> authResultTask = mAuth.signInWithCredential(credential);
-           AuthResult authResult = null;
-           authResult = Tasks.await(authResultTask);
-           sendTokenToDB(authResult.getUser(), context, token);
+           FirebaseUser firebaseUser = firebaseAuthManager.getFirebaseUser(context);
+           sendTokenToDB(firebaseUser, context, token);
         }catch(Exception e){
-            LOGGER.e(e, "error getting firebaseauth user");
+            LOGGER.e(e, "Error registering token");
         }
     }
 
@@ -62,7 +41,7 @@ public class FirebaseUserDataManager {
     }
 
     private FirebaseUserData buildUserData(FirebaseUser firebaseUser, Context context, String token){
-        FirebaseUserData firebaseUserData = null;//getCurrentDataFromFirebase(firebaseUser);
+        FirebaseUserData firebaseUserData = getCurrentDataFromFirebase(firebaseUser);
         if(firebaseUserData == null){
             firebaseUserData = buildNewFirebaseUserData(firebaseUser);
         }
@@ -89,15 +68,21 @@ public class FirebaseUserDataManager {
         return adInfoIdd;
     }
 
-/*    private FirebaseUserData getCurrentDataFromFirebase(FirebaseUser firebaseUser){
+    private FirebaseUserData getCurrentDataFromFirebase(FirebaseUser firebaseUser) {
         FirebaseUserData firebaseUserData = null;
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentSnapshot userDataSnapshot = db.collection("users").document(firebaseUser.getUid()).get().getResult();
-        if(userDataSnapshot != null){
-            firebaseUserData = userDataSnapshot.toObject(FirebaseUserData.class);
+        try {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            Task<DocumentSnapshot> userDataSnapshotTask = db.collection("users").document(firebaseUser.getUid()).get();
+            DocumentSnapshot userDataSnapshot = null;
+            userDataSnapshot = Tasks.await(userDataSnapshotTask);
+            if(userDataSnapshot != null){
+                firebaseUserData = userDataSnapshot.toObject(FirebaseUserData.class);
+            }
+        } catch (Exception e) {
+            LOGGER.e(e, "Error getting current data from firebase");
         }
         return firebaseUserData;
-    }*/
+    }
 
     private FirebaseUserData buildNewFirebaseUserData(FirebaseUser firebaseUser){
         FirebaseUserData userData = null;
@@ -106,8 +91,8 @@ public class FirebaseUserDataManager {
             userData = new FirebaseUserData();
             userData.setId(firebaseUser.getUid());
             userData.setEmail(firebaseUser.getEmail());
-            //List<CameraConfig> cameraConfigList = cameraConfigDao.getAllCameras();
-            //userData.setCameraConfigList(cameraConfigList);
+            List<CameraConfig> cameraConfigList = cameraConfigDao.getAllCameras();
+            userData.setCameraConfigList(cameraConfigList);
         }catch(Exception e){
             LOGGER.e(e, "error building firebase user data");
         }
