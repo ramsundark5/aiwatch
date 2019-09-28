eb1f7f0af03f4f65aa678f21924dcac70a8be835
ram.kuppusamy <Welcome12345&>
2019-09-28 12:07:31 -0400

    use renamed variable in revoke

--- src/settings/GoogleConnectStatusOauth.js
+++ src/settings/GoogleConnectStatusOauth.js
@@ -5,13 +5,13 @@ import { ToastAndroid } from 'react-native';
 import Logger from '../common/Logger';
 
 //webClientId: '119466713568-o59oc7i1d9vr7blopd1396jnhs6cudtn.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+
 const googleOauthConfig = {
   issuer: 'https://accounts.google.com',
   clientId: '119466713568-8eiocl6rns75ab9sdno2r60psa03jdfk.apps.googleusercontent.com',
   redirectUrl: 'com.googleusercontent.apps.119466713568-8eiocl6rns75ab9sdno2r60psa03jdfk:/oauth2redirect/google',
   scopes: ['openid', 'profile','https://www.googleapis.com/auth/drive.file']
 };
-
 export default class GoogleConnectStatusOauth extends Component{ 
   
     async onGoogleAccountSettingsChange(isConnected){
@@ -50,7 +50,7 @@ export default class GoogleConnectStatusOauth extends Component{
     async disconnectGoogleAccount(){
         const { updateSettings, settings } = this.props;
         try{
-          await revoke(config, {
+          await revoke(googleOauthConfig, {
             tokenToRevoke: settings.googleRefreshToken
           });
           updateSettings({ 
