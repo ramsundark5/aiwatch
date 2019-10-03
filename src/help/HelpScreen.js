0c3eaa3783d5da6bd7827ebd0436a3c09fe4e293
ram.kuppusamy <Welcome12345&>
2019-10-03 15:29:42 -0400

    added spinner to faq screen

--- src/help/HelpScreen.js
+++ src/help/HelpScreen.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import LoadingSpinner from '../common/LoadingSpinner';
 
 export default class HelpScreen extends Component{
+    state = {
+        isLoading: false
+    }
+
+    showSpinner(){
+        this.setState({isLoading: true});
+        //to be safe, dismiss the spinner after 2 mins
+        setTimeout(() => {
+            this.setState({isLoading: false});
+        }, 1000 * 120);
+    }
+
+    UNSAFE_componentWillUnmount(){
+        this.setState({isLoading: false});
+    }
+
     render(){
+        const { isLoading } = this.state;
         return(
-            <WebView source={{ uri: 'https://aiwatch.live/help.html' }} />
+            <View style={{flex: 1}}>
+                <WebView source={{ uri: 'https://aiwatch.live/help.html' }} 
+                    onLoadStart={() => this.setState({isLoading: true})}
+                    onLoadEnd={() => this.setState({isLoading: false})}/>
+                <LoadingSpinner
+                    size={30}
+                    animated={true}
+                    cancelable={true}
+                    indeterminate={false}
+                    visible={isLoading}/ >
+            </View>
         )
     }
 }
